73424f8c923ac3268532aaf4fb7d93d82e81921b
徐小小 <1727844832@qq.com>
2021-03-16 18:07:51 +0800

    Button类型、自定义颜色' git pull

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,41 @@
 <template>
   <div id="app">
+    <div>按钮类型</div>
     <div>
-      <x-button @click="buttonClick" type="default" >确认按钮</x-button>
+      <x-button type="default">默认按钮</x-button>
+      <x-button type="primary">主要按钮</x-button>
+      <x-button type="success">成功按钮</x-button>
+      <x-button type="info">信息按钮</x-button>
+      <x-button type="warning">警告按钮</x-button>
+      <x-button type="danger">危险按钮</x-button>
+    </div>
+    <div>自定义按钮颜色</div>
+    <div>
+      <x-button color="#7232dd">单色按钮</x-button>
+      <x-button color="#7232dd" plain>单色按钮</x-button>
+      <x-button color="linear-gradient(to right, #4bb0ff, #6149f6)">
+        渐变色按钮
+      </x-button>
+    </div>
+    <div>
+      <x-button type="default">默认按钮</x-button>
+      <x-button type="primary">主要按钮</x-button>
+      <x-button type="success">成功按钮</x-button>
+      <x-button type="info">信息按钮</x-button>
+      <x-button type="warning">警告按钮</x-button>
+      <x-button type="danger">危险按钮</x-button>
+    </div>
+    <div>禁用按钮</div>
+    <div>
+      <x-button disabled type="default">默认按钮</x-button>
+      <x-button disabled type="primary">主要按钮</x-button>
+      <x-button disabled type="success">成功按钮</x-button>
+      <x-button disabled type="info">信息按钮</x-button>
+      <x-button disabled type="warning">警告按钮</x-button>
+      <x-button disabled type="danger">危险按钮</x-button>
+    </div>
+    <div>
+      <x-button @click="buttonClick" type="default">确认按钮</x-button>
       <x-button type="primary" disabled>普通按钮</x-button>
       <x-button type="success" icon="x-icon-refresh">圆角按钮</x-button>
     </div>
--- src/components/Button/index.vue
+++ src/components/Button/index.vue
@@ -4,12 +4,14 @@
     :class="[
       `x-button--${type}`,
       `x-button--${size}`,
+      `x-button-color--${color}`,
       {
         'is-plain': plain,
         'is-round': round,
         'is-disabled': disabled,
       },
     ]"
+    :style="`background:${color};`"
     @click="handleClick"
     :disabled="disabled"
   >
@@ -28,6 +30,10 @@ export default {
     type: {
       type: String,
       default: 'default'
+    },
+    color: {
+      type: String,
+      default: ''
     },
     plain: {
       type: Boolean,
@@ -55,37 +61,94 @@ export default {
 }
 </script>
 <style scoped>
+.x-button {
+  display: inline-block;
+  line-height: 1;
+  white-space: nowrap;
+  cursor: pointer;
+  background: #fff;
+  border: 1px solid #dcdfe6;
+  color: #606266;
+  -webkit-appearance: none;
+  text-align: center;
+  box-sizing: border-box;
+  outline: none;
+  margin: 0;
+  transition: 0.1s;
+  font-weight: 500;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  padding: 12px 20px;
+  font-size: 14px;
+  border-radius: 4px;
+}
+/* 按钮种类 */
 .x-button--default {
-  color: #000;
-  background-color: #fff;
-  border: 1px solid #000;
 }
 .x-button--success {
-  background-color: green;
-  border: 1px solid green;
+  background-color: #67c23a;
+  border-color: #67c23a;
   color: #fff;
 }
 .x-button--primary {
-  background-color: rgb(72, 134, 192);
-  border: 1px solid rgb(72, 134, 192);
+  background-color: #409eff;
+  border-color: #409eff;
   color: #fff;
 }
+.x-button--info {
+  color: #fff;
+  background-color: #909399;
+  border-color: #909399;
+}
+.x-button--warning {
+  color: #fff;
+  background-color: #e6a23c;
+  border-color: #e6a23c;
+}
+.x-button--danger {
+  color: #fff;
+  background-color: #f56c6c;
+  border-color: #f56c6c;
+}
+[class*="x-button-color--"]{
+ color: #fff;
+ border: 0;
+}
+.is-plain{
+    background-color: #fff;
+}
+/* 朴素按钮 */
 .x-button--success.is-plain {
-  background-color: #fff;
-  color: green;
+  color: #67c23a;
 }
 .x-button--primary.is-plain {
-  background-color: #fff;
-  color: rgb(72, 134, 192);
+  color: #409eff;
 }
+.x-button--info.is-plain {
+  color: #909399;
+}
+.x-button--warning.is-plain {
+  color: #e6a23c;
+}
+.x-button--danger.is-plain {
+  color: #f56c6c;
+}
+/* 圆角按钮 */
 .x-button.is-round {
   border-radius: 30px;
 }
+/* 按钮图标 */
 .x-button [class*="x-icon-"] + span {
   margin-left: 5px;
 }
-.x-button.is-disabled{
+[class*="x-button-"]:focus, [class*="x-button-"]:hover {
+ opacity: 0.8;
+}
+/* 禁用按钮 */
+.x-button.is-disabled {
   opacity: 0.5;
+  cursor: not-allowed;
 }
 </style>
 ++
