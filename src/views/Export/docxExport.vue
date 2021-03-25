11b01d91aeb22d2d0496cc2867b89586a04b4bf2
徐小小 <1727844832@qq.com>
2021-03-25 17:04:48 +0800

    添加word导出

---
+++ src/views/Export/docxExport.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="docxExport">
+    <div @click="exportTable">
+      点击下载报表
+    </div>
+  </div>
+</template>
+<script>
+import { exportDocx } from '@/utils/docx'
+export default {
+  name: 'docxExport',
+  data () {
+    return {}
+  },
+  methods: {
+    exportTable (e) {
+      const data = {
+        custName: '客户1',
+        phoneNumber: '11111111111',
+        projectRequirement: '要求',
+        remark: '备注',
+        totalPrice: '10000',
+        checkReason: '原因',
+        table: [
+          {
+            number: 1, // 序号
+            name: '设备1', // 设备名称
+            num: 1, // 数量
+            salePrice: 10, // 销售单价
+            saleTotal: 10, // 销售合计
+            remark: '啦啦啦' // 备注
+          },
+          {
+            number: 2, // 序号
+            name: '设备2', // 设备名称
+            num: 2, // 数量
+            salePrice: 20, // 销售单价
+            saleTotal: 40, // 销售合计
+            remark: '啦啦啦' // 备注
+          },
+          {
+            number: 3, // 序号
+            name: '设备3', // 设备名称
+            num: 3, // 数量
+            salePrice: 30, // 销售单价
+            saleTotal: 90, // 销售合计
+            remark: '啦啦啦' // 备注
+          }
+        ]
+      }
+      exportDocx('/static/input.docx', data, `报价.docx`)
+    }
+  }
+}
+</script>
+<style>
+#docxExport {
+  text-align: right;
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,12 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div id="Home">
+    <div>按钮</div>
+    <div>导出</div>
   </div>
 </template>
 <script>
 export default {
-  name: 'App',
+  name: 'Home',
   data () {
     return {}
   },
@@ -13,7 +14,7 @@ export default {
 }
 </script>
 <style>
-#App {
+#Home {
   text-align: right;
 }
 </style>
